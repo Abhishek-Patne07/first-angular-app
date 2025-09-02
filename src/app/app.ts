99c020e311302a8a17b0bbcea8f095a8b659f837
Abhishek Patane <Abhishekpatne071002@gmail.com>
2025-09-02 15:34:21 +0530

    Add project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';  
+import {User} from "./user/user";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,   // make sure this is standalone
+  imports: [RouterOutlet, HeaderComponent , User],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/user/user.ts
@@ -0,0 +1,26 @@
+import { Component , signal , computed} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrls: ['./user.css']
+})
+export class User {
+   selectedUser = signal( DUMMY_USERS[randomIndex]);
+    // imagePath = computed(() => 'assets/users/' +  this.selectedUser().avatar)
+   get imagePath(){
+    return 'assets/users/' + this.selectedUser().avatar; 
+    }
+
+    onSelectUser(){
+      const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+            this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
+      // this.selectedUser = DUMMY_USERS[randomIndex];
+    }
+}
+
+
