3b9288b8e68fa862c75981b48b64e897b208e8c5
Abhishek Patane <Abhishekpatne071002@gmail.com>
2025-09-03 15:37:53 +0530

    add multiple user

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';  
 import {User} from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,   // make sure this is standalone
-  imports: [RouterOutlet, HeaderComponent , User],  
+  imports: [RouterOutlet , HeaderComponent , User],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,6 +1,4 @@
-import { Component , signal , computed} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,18 +7,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrls: ['./user.css']
 })
 export class User {
-   selectedUser = signal( DUMMY_USERS[randomIndex]);
-    // imagePath = computed(() => 'assets/users/' +  this.selectedUser().avatar)
-   get imagePath(){
-    return 'assets/users/' + this.selectedUser().avatar; 
-    }
+  @Input() avatar!: string;
+  @Input() name!: string;
+  
+  get imagePath() {
+    return 'assets/users/ ' +this.avatar;
+  }
+   onSelectedUser(){
 
-    onSelectUser(){
-      const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-            this.selectedUser.set(DUMMY_USERS[randomIndex]);
-
-      // this.selectedUser = DUMMY_USERS[randomIndex];
-    }
+   }
+   
 }
 
 
